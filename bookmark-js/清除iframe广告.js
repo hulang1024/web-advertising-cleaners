1b7d70334d966ee7eb5a8009349f0af3a03dcb55
hulang1024 <1013644379@qq.com>
2018-06-09 17:56:39 +0800

    .

---
+++ bookmark-js/清除iframe广告.js
@@ -0,0 +1,40 @@
+(function() {
+  clean();
+  document.addEventListener('load', clean);
+
+  var timer = setInterval(function() {
+    if (iframeAreAllEmpty())
+      clearInterval(timer);
+    clean();
+  }, 1000);
+
+  function iframeAreAllEmpty() {
+    for (var i = 0, a = allIframes(); i < a.length; i++) {
+      if (!(a[i].src || a[i].contentWindow.document.body.children.length))
+        return false;
+    }
+    return true;
+  }
+
+  function allIframes() {
+    var iframes = document.getElementsByTagName('iframe');
+    return Array.prototype.slice.call(iframes, 0);
+  }
+
+  function clean() {
+    allIframes().forEach(function(iframe){
+      try {
+        iframe.style.visibility = 'hidden';
+        iframe.parentElement.style.visibility = 'hidden';
+        iframe.src = '';
+        var clearHTML = function() {
+          iframe.contentWindow.document.body.innerHTML = '';
+        };
+        clearHTML();
+        iframe.addEventListener('load', function() {
+          clearHTML();
+        });
+      } catch(e) {}
+    });
+  }
+})();
--- bookmarks-js/清除iframe广告.js
+++
@@ -1,7 +0,0 @@
-var iframes = document.getElementsByTagName('iframe');
-Array.prototype.slice.call(iframes, 0).forEach(function(iframe){
-  try {
-    iframe.contentWindow.document.body.innerHTML = '';
-    iframe.src = '';
-  } catch(e) {}
-});
